bdc56df37e0830906e749b8470822a756d6c3624
Pericles <slouis482@gmail.com>
2025-05-07 14:48:43 -0400

    ajuste

--- config/config.js
+++ config/config.js
@@ -1,15 +1,6 @@
 require('dotenv').config(); // Cargar variables de entorno
 
 module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_SERVER,
-    port: process.env.DB_PORT,
-    dialect: 'mysql',
-    dialectModule: require('mysql2')  // Especificamos el uso de mysql2
-  },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
